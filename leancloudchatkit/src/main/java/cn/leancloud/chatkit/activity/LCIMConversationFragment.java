958ef12f5d5c8cecdd82f7e2269db25a00a2be24
daweibayu <daweibayu@gmail.com>
2016-09-12 15:25:19 +0800

    fix npe

--- leancloudchatkit/src/main/java/cn/leancloud/chatkit/activity/LCIMConversationFragment.java
+++ leancloudchatkit/src/main/java/cn/leancloud/chatkit/activity/LCIMConversationFragment.java
@@ -320,16 +320,24 @@ public class LCIMConversationFragment extends Fragment {
    * @return
    */
   private String getRealPathFromURI(Context context, Uri contentUri) {
-    Cursor cursor = null;
-    try {
-      String[] proj = {MediaStore.Images.Media.DATA};
-      cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
-      int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-      cursor.moveToFirst();
-      return cursor.getString(column_index);
-    } finally {
-      if (cursor != null) {
-        cursor.close();
+    if (contentUri.getScheme().equals("file")) {
+      return contentUri.getEncodedPath();
+    } else {
+      Cursor cursor = null;
+      try {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+        if (null != cursor) {
+          int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+          cursor.moveToFirst();
+          return cursor.getString(column_index);
+        } else {
+          return "";
+        }
+      } finally {
+        if (cursor != null) {
+          cursor.close();
+        }
       }
     }
   }
