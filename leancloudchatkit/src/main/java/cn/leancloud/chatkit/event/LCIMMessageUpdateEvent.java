80796e772cdf4ebc3be95dc412d99fe762abac2d
daweibayu <daweibayu@gmail.com>
2017-07-04 17:57:41 +0800

    support message recall

---
+++ leancloudchatkit/src/main/java/cn/leancloud/chatkit/event/LCIMMessageUpdateEvent.java
@@ -0,0 +1,11 @@
+package cn.leancloud.chatkit.event;
+
+import com.avos.avoscloud.im.v2.AVIMMessage;
+
+/**
+ * Created by wli on 2017/6/28.
+ */
+
+public class LCIMMessageUpdateEvent {
+  public AVIMMessage message;
+}
