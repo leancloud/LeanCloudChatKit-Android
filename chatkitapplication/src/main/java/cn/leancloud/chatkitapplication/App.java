21d60df2103dbab8f7f538f4f0ab16d70f22142b
jwfing <jwfing@gmail.com>
2017-09-02 14:33:05 +0800

    test for intviu rtm proxy

--- chatkitapplication/src/main/java/cn/leancloud/chatkitapplication/App.java
+++ chatkitapplication/src/main/java/cn/leancloud/chatkitapplication/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.avos.avoscloud.AVOSCloud;
 import com.avos.avoscloud.im.v2.AVIMClient;
+import com.avos.avoscloud.im.v2.AVIMOptions;
 
 import cn.leancloud.chatkit.LCChatKit;
 
@@ -16,12 +17,19 @@ public class App extends Application {
   private final String APP_ID = "dYRQ8YfHRiILshUnfFJu2eQM-gzGzoHsz";
   private final String APP_KEY = "ye24iIK6ys8IvaISMC4Bs5WK";
 
+  // 请替换成小班会产品的 appid 和 appkey
+  private final String APP_ID_XIAOBANHUI = "";
+  private final String APP_KEY_XIAOBANHUI = "";
+
   @Override
   public void onCreate() {
     super.onCreate();
     LCChatKit.getInstance().setProfileProvider(CustomUserProvider.getInstance());
     AVOSCloud.setDebugLogEnabled(true);
-    LCChatKit.getInstance().init(getApplicationContext(), APP_ID, APP_KEY);
+    AVOSCloud.setLogLevel(AVOSCloud.LOG_LEVEL_VERBOSE);
+    AVOSCloud.setServer(AVOSCloud.SERVER_TYPE.API, "https://intviu-api.leancloud.cn");
+    AVIMOptions.getGlobalOptions().setRTMServer("wss://intviu-rtm.leancloud.cn");
+    LCChatKit.getInstance().init(getApplicationContext(), APP_ID_XIAOBANHUI, APP_KEY_XIAOBANHUI);
     AVIMClient.setAutoOpen(false);
   }
 }
